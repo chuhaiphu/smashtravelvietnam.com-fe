0f97f0fee37e23b67b9464f3c7babd52b53feb6f
doanvinhphu <doanvinhphu321@gmail.com>
2026-03-22 19:10:58 +0700

    Improve landing blog and tour category components with sorting logic. Fix media image modal image fetching bug.

--- src/components/primitives/landing-tour-category/landing-tour-category.tsx
+++ src/components/primitives/landing-tour-category/landing-tour-category.tsx
@@ -1,10 +1,10 @@
-import { getTourCategoryToursByTourCategoryIdAction } from "@/actions/tour-category-tour-action";
-import TourGrid from "@/components/grids/tour-grid/tour-grid";
-import { ITourCategoryResponse } from "@/interfaces/tour-category-interface";
-import { ITourResponse } from "@/interfaces/tour-interface";
-import { Stack } from "@mantine/core";
-import VideoSection from "@/components/primitives/video-section/video-section";
-import classes from "./landing-tour-category.module.scss";
+import { getTourCategoryToursByTourCategoryIdAction } from '@/actions/tour-category-tour-action';
+import TourGrid from '@/components/grids/tour-grid/tour-grid';
+import { ITourCategoryResponse } from '@/interfaces/tour-category-interface';
+import { ITourResponse } from '@/interfaces/tour-interface';
+import { Stack } from '@mantine/core';
+import VideoSection from '@/components/primitives/video-section/video-section';
+import classes from './landing-tour-category.module.scss';
 
 interface LandingTourCategoryProps {
   category: ITourCategoryResponse;
@@ -15,43 +15,73 @@ interface LandingTourCategoryProps {
   };
 }
 
-export default async function LandingTourCategory({ category, queryParams }: LandingTourCategoryProps) {
+export default async function LandingTourCategory({
+  category,
+  queryParams,
+}: LandingTourCategoryProps) {
   // Get all tours in this category
-  const tourCategoryToursResponse = await getTourCategoryToursByTourCategoryIdAction(category.id);
+  const tourCategoryToursResponse =
+    await getTourCategoryToursByTourCategoryIdAction(category.id);
 
   // Extract tours and filter only public ones
-  const tours: ITourResponse[] = tourCategoryToursResponse.success && tourCategoryToursResponse.data
-    ? tourCategoryToursResponse.data
-      .map(tct => tct.tour)
-      .filter((tour): tour is ITourResponse => tour !== undefined && tour.visibility === 'public')
-    : [];
+  const toursFromTourCategory: ITourResponse[] =
+    tourCategoryToursResponse.success && tourCategoryToursResponse.data
+      ? tourCategoryToursResponse.data
+          .map((tct) => tct.tour)
+          .filter(
+            (tour): tour is ITourResponse =>
+              tour !== undefined && tour.visibility === 'public'
+          )
+      : [];
+
+  const sortedTours = toursFromTourCategory
+    .sort(
+      (a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()
+    )
+    .sort((a, b) => a.sortOrder - b.sortOrder);
 
   const renderHTMLDescription = (htmlDescription: string | null) => {
-    if (!htmlDescription || htmlDescription.trim() === '' || htmlDescription.trim() === '<p></p>') {
+    if (
+      !htmlDescription ||
+      htmlDescription.trim() === '' ||
+      htmlDescription.trim() === '<p></p>'
+    ) {
       return null;
     }
     return (
-      <div dangerouslySetInnerHTML={{ __html: htmlDescription }} className={classes.htmlDescription}></div>
+      <div
+        dangerouslySetInnerHTML={{ __html: htmlDescription }}
+        className={classes.htmlDescription}
+      ></div>
     );
-  }
-  const renderVideoSection = (videoUrl?: string, thumbnailUrl?: string, title?: string) => {
+  };
+  const renderVideoSection = (
+    videoUrl?: string,
+    thumbnailUrl?: string,
+    title?: string
+  ) => {
     if (!videoUrl) {
-      return <></>
+      return <></>;
     }
     return (
-      <VideoSection url={videoUrl}
+      <VideoSection
+        url={videoUrl}
         title={title}
-        height={480} thumbnailUrl={thumbnailUrl || undefined} />
+        height={480}
+        thumbnailUrl={thumbnailUrl || undefined}
+      />
     );
-  }
+  };
 
   return (
     <Stack gap={'xs'} mb={'lg'}>
       <h1 className={classes.sectionTitle}>{category.title}</h1>
       {category.videoPosition === 'top' &&
-        renderVideoSection(category.videoUrl || undefined,
+        renderVideoSection(
+          category.videoUrl || undefined,
           category.videoThumbnailUrl || undefined,
-          category.title)}
+          category.title
+        )}
       {renderHTMLDescription(category.description)}
       <TourGrid
         queryParams={{
@@ -59,13 +89,14 @@ export default async function LandingTourCategory({ category, queryParams }: Lan
           type: queryParams.type,
           destinations: queryParams.destinations,
         }}
-        toursData={tours}
+        toursData={sortedTours}
       />
       {category.videoPosition === 'bottom' &&
-        renderVideoSection(category.videoUrl || undefined,
+        renderVideoSection(
+          category.videoUrl || undefined,
           category.videoThumbnailUrl || undefined,
-          category.title)}
+          category.title
+        )}
     </Stack>
   );
 }
-
--- src/components/primitives/upload-image-section/media-image-modal.tsx
+++ src/components/primitives/upload-image-section/media-image-modal.tsx
@@ -1,10 +1,15 @@
 'use client';
 import '@vinaup/media-ui/dist/index.css';
-import { MediaModal , type IMedia, type UploadResult, type ICreateMedia } from '@vinaup/media-ui';
-import { uploadImageAction } from "@/actions/upload-action";
-import { createManyMediaAction, getAllMediaAction } from "@/actions/media-action";
+import {
+  MediaModal,
+  type IMedia,
+  type UploadResult,
+  type ICreateMedia,
+} from '@vinaup/media-ui';
+import { uploadImageAction } from '@/actions/upload-action';
+import { createManyMediaAction, getAllMediaAction } from '@/actions/media-action';
 import { useState, useEffect, useEffectEvent } from 'react';
-import { notifications } from "@mantine/notifications";
+import { notifications } from '@mantine/notifications';
 
 interface MediaImageModalProps {
   opened: boolean;
@@ -21,50 +26,56 @@ export default function MediaImageModal({
 
   const fetchAvailableImages = useEffectEvent(async () => {
     const response = await getAllMediaAction();
-    const imagesList = response.data?.filter((media) => media.type === 'image') as IMedia[];
+    const imagesList = response.data?.filter(
+      (media) => media.type === 'image'
+    ) as IMedia[];
     setAvailableImages(imagesList);
   });
 
   useEffect(() => {
-    fetchAvailableImages();
-  }, []);
+    if (opened) {
+      fetchAvailableImages();
+    }
+  }, [opened]);
 
   const handleUpload = async (files: File[]): Promise<UploadResult[]> => {
     const successResults: UploadResult[] = [];
-    
+
     // Upload sequentially to ensure unique file names
     for (const file of files) {
       const uploadResponse = await uploadImageAction(file, 'media');
       if (uploadResponse.success && uploadResponse.data) {
         successResults.push({
           url: uploadResponse.data,
-          name: file.name
+          name: file.name,
         });
       }
     }
-    
+
     if (successResults.length === 0 && files.length > 0) {
-      throw new Error("All files failed to upload.");
+      throw new Error('All files failed to upload.');
     }
     return successResults;
-  }
+  };
 
   const handleSave = async (data: ICreateMedia[]): Promise<IMedia[]> => {
     const response = await createManyMediaAction(data);
     if (!response.success || !response.data) {
-      throw new Error(response.error || "There was an error saving to the database");
+      throw new Error(
+        response.error || 'There was an error saving to the database'
+      );
     }
     return response.data as unknown as IMedia[];
-  }
+  };
 
   const handleUploadSuccess = (media: IMedia[]) => {
-    setAvailableImages(prev => [...prev, ...media]);
+    setAvailableImages((prev) => [...prev, ...media]);
     notifications.show({
       title: 'Upload success',
       message: `Upload success ${media.length} images`,
       color: 'green',
     });
-  }
+  };
 
   const handleUploadError = (error: Error) => {
     notifications.show({
@@ -72,7 +83,7 @@ export default function MediaImageModal({
       message: error.message || 'There was an error',
       color: 'red',
     });
-  }
+  };
   return (
     <MediaModal
       opened={opened}
